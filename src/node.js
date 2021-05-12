a85ed0e8501d29355b4f4c76565d398ebb3b1cf9
Pavel Denisjuk <pavel@webiny.com>
2021-05-12 14:00:47 +0200

    fix: class capitalization

--- src/node.js
+++ src/node.js
@@ -4,10 +4,10 @@ const { WTSCore } = require("./core");
 /**
  * Use `node-fetch`, as we already use it in other Webiny packages, so it won't add to the bundle size.
  */
-class Wts extends WTSCore {
+class WTS extends WTSCore {
     constructor(config = {}) {
         super({ FETCH: fetch, ...config });
     }
 }
 
-module.exports = { Wts };
+module.exports = { WTS };

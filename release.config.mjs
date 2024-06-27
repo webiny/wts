e0000429cb2ec42e97ebdb88e907135969233da6
adrians5j <adrian@webiny.com>
2024-06-27 13:21:43 +0200

    fix: build code into `dist` and only publish that to NPM

--- release.config.mjs
+++ release.config.mjs
@@ -2,5 +2,11 @@
  * @type {import('semantic-release').GlobalConfig}
  */
 export default {
-  branches: ["v2"]
+    branches: ["v2"],
+    plugins: [
+        "@semantic-release/commit-analyzer",
+        "@semantic-release/release-notes-generator",
+        ["@semantic-release/npm", {"pkgRoot": "./dist"}],
+        "@semantic-release/github"
+    ]
 };

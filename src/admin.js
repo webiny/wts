5552d39f775c82a741f38c60be4c4aac5327e8ee
Bruno Zorić <bruno.zoric@gmail.com>
2025-09-20 20:25:17 +0200

    refactor: as module

--- src/admin.js
+++ src/admin.js
@@ -1,18 +1,11 @@
-"use strict";
+import { WTSCore } from "./core.js";
 
-const { WTSCore } = require("./core");
-
-/**
- * Use `node-fetch`, as we already use it in other Webiny packages, so it won't add to the bundle size.
- */
-class WTS extends WTSCore {
+export class WTS extends WTSCore {
   constructor(config = {}) {
     super({
       FETCH: fetch.bind(window),
       BTOA: btoa.bind(window),
-      ...config
+      ...config,
     });
   }
 }
-
-module.exports = { WTS };
